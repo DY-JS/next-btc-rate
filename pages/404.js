8bded6eecb0701442e05becfb215c4c9b6c46310
DY-JS <delo1604@ukr.net>
2022-08-08 12:42:22 +0300

    Add Error process

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,18 @@
 import Image from 'next/image';
+import errorImg from '../public/icon-error-404.png';
 import styles from '../styles/404.module.scss';
 
 export default function Error() {
   return (
     <div className={styles.error}>
       <p>Something went wrong...</p>
-      <Image src="/icon-error-404.png" width={360} height={360} alt="Logo" />
+      <Image
+        src={errorImg}
+        width={360}
+        height={360}
+        alt="Logo"
+        placeholder="blur"
+      />
     </div>
   );
 }
